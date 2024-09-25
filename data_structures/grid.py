79467884f8ed9c5257ad50cc3ad2f51e733f2fd3
dr1602 <david.arangelg@gmail.com>
2024-09-24 19:24:38 -0600

    random_grid

--- data_structures/grid.py
+++ data_structures/grid.py
@@ -1,11 +1,12 @@
 from set_array import Array
+import random
 
 class Grid():
     def __init__(self, rows, columns, fill_value=None):
         self.data = Array(rows)
         for row in range(rows):
             self.data[row] = Array(columns, fill_value)
-            
+
     def get_height(self):
         return len(self.data)
     
@@ -25,7 +26,29 @@ class Grid():
             result += '\n'
                 
         return str(result)
+    
+                
+    def random_number_fill(self, lower_limit=0, higher_limit=10):
+        for row in range(self.get_height()):
+            for column in range(self.get_width()):
+                self[row][column] = random.randint(int(lower_limit), int(higher_limit))
+                
+        return self
+        
+    def random_letters_fill(self):
+        for row in range(self.get_height()):
+            for column in range(self.get_width()):
+                self[row][column] = random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    
+        return self
             
+    def random_string_fill(self):
+        for row in range(self.get_height()):
+            for column in range(self.get_width()):
+                self[row][column] = random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
+        
+        return self
+    
 if __name__ == '__main__':
     from grid import Grid
     
@@ -57,4 +80,24 @@ if __name__ == '__main__':
     0 1 2 
     0 2 4 
     '''
+    
+    matrix_two = Grid(3, 3)
+    print(f'this the matrix two: \n{matrix_two}')
+    
+    '''
+    this the matrix two: 
+    None None None 
+    None None None 
+    None None None 
+    '''
+    
+    print(f'this the matrix two filled with numbers: \n{matrix_two.random_number_fill()}')
+    
+    print(f'this the matrix two filled with letters: \n{matrix_two.random_letters_fill()}')
+    
+    print(f'this the matrix two filled with numbers and/ or letters as strings: \n{matrix_two.random_string_fill()}')
+
+    
+
+
     
